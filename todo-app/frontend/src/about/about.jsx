f4afe9a566ecf52632bb8f182d354b6aff4f90a1
Julio Cesar <julioprogrammer@Julios-Mac.local>
2017-07-15 18:34:29 -0300

    Seção 5 - Aula 49

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+const About = props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Valores</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+);
+
+export default About;
