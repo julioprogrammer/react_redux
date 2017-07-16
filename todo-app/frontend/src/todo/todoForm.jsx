de08f729838e7832a399706bab3080e0c39bb66d
Julio Cesar <julioprogrammer@Julios-Mac.local>
2017-07-16 18:06:52 -0300

    Seção 5 - Aula 55

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
 const TodoForm = props => (
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 3 2'>
+            <input id='description' className='form-control' 
+                placeholder='Adicione uma tarefa'
+                onChange={ props.handleChange }
+                value={ props.description } />
+        </Grid>
+
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+                onClick={ props.handleAdd }></IconButton>
+        </Grid>
     </div>
 );
 
