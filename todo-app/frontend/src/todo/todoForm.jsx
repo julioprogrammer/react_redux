04bc64542419ecf308190a2802e49bb64966dd8b
julio cesar <julio.cesar@soaexpert.com.br>
2017-07-22 09:54:36 -0300

    Seção 7 - Aula 73

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
@@ -33,4 +34,6 @@ const TodoForm = props => {
     );
 }
 
-export default TodoForm;
+const mapStateToProps = state => ({ description: state.todo.description });
+
+export default connect(mapStateToProps)(TodoForm);
